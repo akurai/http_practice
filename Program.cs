008d5aea56c6d87d08401d504f3ae635a9034860
antal <antal.kurai@gmail.com>
2021-06-21 20:52:28 +0200

    generated dotnet console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_network_stuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
