a13ff94261537c0d8eaa058f7a734443790980cc
akurai <antal.kurai@gmail.com>
2021-06-22 17:10:41 +0200

    updated with line

--- updateAndRun.ps1
+++ updateAndRun.ps1
@@ -1,3 +1,4 @@
 cls
 git pull http://github.com/akurai/http_practice main
+Write-Host "**************************************** starting program ****************************************"
 dotnet run
