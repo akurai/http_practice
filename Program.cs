35471c4e9842aa13a36833c7a324196507d66311
akurai <antal.kurai@gmail.com>
2021-06-22 17:21:06 +0200

    a

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace csharp_network_stuff
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 100; i++)
             {
                 System.Console.WriteLine(i + " db kismalac jart itt! ");
             }
