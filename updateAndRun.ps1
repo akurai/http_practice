b8d780deabdbee612aba4f5822346e702f06581c
antal <antal.kurai@gmail.com>
2021-06-23 21:47:32 +0200

    auto updater v1.0

--- updateAndRun.ps1
+++ updateAndRun.ps1
@@ -4,7 +4,5 @@ rm ./updateAndRun.ps1
 git pull --set-upstream http://github.com/akurai/http_practice main
 git branch -m master main
 
-
-
 Write-Host "**************************************** starting program ****************************************"
 dotnet run
