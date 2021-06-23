d590f556e269fd655e29e50275abaa28d285fdb4
antal <antal.kurai@gmail.com>
2021-06-23 23:51:12 +0200

    added installer v1.0.1

--- install.ps1
+++ install.ps1
@@ -1,10 +1,15 @@
 cls
 if(Test-Path -Path './.git' -PathType Container ){
-    rm ./.git -r -fo
+    if((Read-Host "!!!WARNING!!! this will delete all files in this directory! Continue? [y/n]") -eq "y"){
+        rm ./* -r -fo
+    }
+    
 }
+
 git init
 git pull --set-upstream http://github.com/akurai/http_practice main
 git branch -m master main
 
 Write-Host "**************************************** starting program ****************************************"
 dotnet run
+
