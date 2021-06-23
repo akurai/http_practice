1884cf5f5ec75f4780c902c0ea26e48304e97d8b
antal <antal.kurai@gmail.com>
2021-06-23 23:57:52 +0200

    added installer v1.0.1

--- install.ps1
+++ install.ps1
@@ -3,7 +3,12 @@ if(Test-Path -Path './.git' -PathType Container ){
     if((Read-Host "!!!WARNING!!! this will delete all files in this directory! Continue? [y/n]") -eq "y"){
         rm ./* -r -fo
     }
-    
+    else{
+        exit
+    }
+}
+else{
+    rm ./install.ps1
 }
 
 git init
