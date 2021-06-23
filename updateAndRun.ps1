e99b7fb82977c0c872f886d66780e7a429970f82
antal <antal.kurai@gmail.com>
2021-06-23 20:26:28 +0200

    updater updated

--- updateAndRun.ps1
+++ updateAndRun.ps1
@@ -1,5 +1,5 @@
 cls
 rm ./updateAndRun.ps1
-git pull -u http://github.com/akurai/http_practice main
+git pull --set-upstream http://github.com/akurai/http_practice main
 Write-Host "**************************************** starting program ****************************************"
 dotnet run
