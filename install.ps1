2d406da51423eadbd5de4328d91a97b240c4ee16
akurai <antal.kurai@gmail.com>
2021-06-24 11:56:15 +0200

    made conti proxy compatible

--- install.ps1
+++ install.ps1
@@ -1,4 +1,5 @@
 # curl "https://raw.githubusercontent.com/akurai/http_practice/main/install.ps1" > install.ps1
+
 cls
 if(Test-Path -Path './.git' -PathType Container ){
     if((Read-Host "!!!WARNING!!! this will delete all files in this directory! Continue? [y/n]") -eq "y"){
@@ -13,6 +14,11 @@ else{
 }
 
 git init
+$param = $args[0]
+if($param -eq "conti"){
+    git config --global http.proxy http://199.19.250.205:80
+    git config --global https.proxy https://199.19.250.205:80
+}
 git pull --set-upstream http://github.com/akurai/http_practice main
 git branch -m master main
 
